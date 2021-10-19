b1cc97b369f0a71ba5f92ae362e7a422328f339a
RuhiuEdwin <ruhiuedwin1@gmail.com>
2021-10-19 14:56:28 +0300

    all new

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar_menu')
+const navLog = document.querySelector('#navbar_logo')
+const body = document.querySelector('body')
+
+//display mobile menu
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+    body.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenu)
+
